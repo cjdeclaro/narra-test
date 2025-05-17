fbd00a7efb0042907d5f0109873a122e9f47e4ef
cjdeclaro <cjdeclaro16@gmail.com>
2025-05-17 16:44:59 +0800

    feat: implement Grader class with basic CSV parsing from file input

--- lib/grader.rb
+++ lib/grader.rb
@@ -1,7 +1,28 @@
-require "csv"
-require "student"
+require 'csv'
 
 class Grader
-  def initialize(file)
+  attr_reader :students
+
+  def initialize(csv_path)
+    @csv_path = csv_path
+    @students = []
+    parse_csv
+  end
+
+  private
+
+  def parse_csv
+    csv_data = CSV.read(@csv_path, headers: true)
+    csv_data.each do |row|
+      student = {
+        name: row['Name'],
+        grades: {
+          english: row['English'].to_i,
+          math: row['Math'].to_i,
+          physics: row['Physics'].to_i
+        }
+      }
+      @students << student
+    end
   end
 end
