554dfb8c091ba365f61c8bef357f3de96b756cf7
cjdeclaro <cjdeclaro16@gmail.com>
2025-05-17 17:09:49 +0800

    feat: implement CSV parsing and store students with grades as integers

--- lib/grader.rb
+++ lib/grader.rb
@@ -3,25 +3,27 @@ require 'csv'
 class Grader
   attr_reader :students
 
-  def initialize(csv_path)
-    @csv_path = csv_path
+  def initialize(file)
     @students = []
-    parse_csv
+    parse_csv(file)
   end
 
   private
 
-  def parse_csv
-    csv_data = CSV.read(@csv_path, headers: true)
-    csv_data.each do |row|
-      student = {
-        name: row['Name'],
-        grades: {
-          english: row['English'].to_i,
-          math: row['Math'].to_i,
-          physics: row['Physics'].to_i
-        }
+  def parse_csv(file)
+    CSV.new(file, headers: true, header_converters: :symbol).each do |row|
+      grades = {
+        english: row[:english].to_i,
+        math: row[:math].to_i,
+        physics: row[:physics].to_i
       }
+
+      student = {
+        name: row[:name],
+        grades: grades,
+        average: 0
+      }
+
       @students << student
     end
   end
